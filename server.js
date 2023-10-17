aa57a70528e9a3bcb793088cfc20a0cf254348cd
hw1010 <hyewon6588@naver.com>
2023-10-16 20:19:12 -0400

    updated server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import app from './server/express.js'
 import mongoose from 'mongoose' 
 mongoose.Promise = global.Promise
 mongoose.connect(config.mongoUri, { useNewUrlParser: true,
-useCreateIndex: true, 
+// useCreateIndex: true, 
 useUnifiedTopology: true } )
 mongoose.connection.on('error', () => {
-throw new Error(`unable to connect to database: ${mongoUri}`) 
+throw new Error(`unable to connect to database: ${config.mongoUri}`) 
 })
 app.get("/", (req, res) => {
 res.json({ message: "Welcome to DressStore application." });
