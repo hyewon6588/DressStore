f4b55389baedbc8042610baa054bfe75bcb35a96
hw1010 <hyewon6588@naver.com>
2023-10-16 22:12:48 -0400

    added product.model.js,dbErrorHandler.js

---
+++ server/models/product.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+ name: {
+  type: String,
+  trim: true,
+  required: 'Name is required'
+ },
+ description: {
+  type: String,
+  trim: true,
+  required: 'Description is required'
+ },
+ price: {
+  type: Number,
+  required:'Price is required'
+ },
+ published: {
+  type: Boolean,
+  default: true
+ },
+ category: {
+  type: String,
+  unique:'category already exists',
+  required: 'Category is required'
+ }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
