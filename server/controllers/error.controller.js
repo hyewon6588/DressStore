746acc17a8e31e18c01e943fac660086acae5189
hw1010 <hyewon6588@naver.com>
2023-10-16 23:53:51 -0400

    updated error controller files

--- server/controllers/error.controller.js
+++ server/controllers/error.controller.js
@@ -2,10 +2,11 @@
 // Import any necessary modules or dependencies
 // Example: const SomeModule = require('some-module');
 // Define your controller function
-function handleError(req, res) {
-     // Your code to handle the error
+function getErrorMessage(errMsg) {
+    console.log(errMsg);
     }
     // Export the controller function
-    module.exports = {
-     handleError: handleError
+    export default  {
+      handleError: handleError,
+      getErrorMessage:getErrorMessage
     };
