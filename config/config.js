2344891dafec0fd4964456b5ede8eb61a10b4c9e
hw1010 <hyewon6588@naver.com>
2023-10-16 20:17:21 -0400

    updated config.js

--- config/config.js
+++ config/config.js
@@ -1,11 +1,24 @@
+// const config = {
+//     env: process.env.NODE_ENV || 'development', 
+//     port: process.env.PORT || 3000,
+//     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+//     mongoUri: process.env.MONGODB_URI ||"mongodb+srv://hyewon6588:SHq5DW2qSwe3IrER@dressstore.bvk0d3k.mongodb.net/?retryWrites=true&w=majority"||
+//     process.env.MONGO_HOST ||
+//     'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+//    (process.env.MONGO_PORT || '27017') +
+//     '/mernproject'
+//     }
+// export default config
+
+require("dotenv").config();
 const config = {
-    env: process.env.NODE_ENV || 'development', 
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI ||"mongodb+srv://hyewon6588:SHq5DW2qSwe3IrER@dressstore.bvk0d3k.mongodb.net/?retryWrites=true&w=majority"||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' + 
-   (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
-    }
+env: process.env.NODE_ENV || 'development', 
+port: process.env.PORT || 3000,
+jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+mongoUri: process.env.MONGODB_URI || "mongodb+srv://hyewon6588:SHq5DW2qSwe3IrER@dressstore.bvk0d3k.mongodb.net/?retryWrites=true&w=majority"||
+process.env.MONGO_HOST ||
+'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+(process.env.MONGO_PORT || '27017') +
+'/mernproject' 
+}
 export default config
