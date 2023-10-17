cb8a13821ad152e6dd6f94ee1c75149acd4a8136
hw1010 <hyewon6588@naver.com>
2023-10-16 20:26:01 -0400

    updated config.js

--- config/config.js
+++ config/config.js
@@ -10,12 +10,13 @@
 //     }
 // export default config
 
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 const config = {
 env: process.env.NODE_ENV || 'development', 
 port: process.env.PORT || 3000,
 jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-mongoUri: process.env.MONGODB_URI || "mongodb+srv://hyewon6588:SHq5DW2qSwe3IrER@dressstore.bvk0d3k.mongodb.net/?retryWrites=true&w=majority"||
+mongoUri: process.env.MONGODB_URI ||
 process.env.MONGO_HOST ||
 'mongodb://' + (process.env.IP || 'localhost') + ':' + 
 (process.env.MONGO_PORT || '27017') +
