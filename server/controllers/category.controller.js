3aac3e0cb36dc002351a18a8168cae685f5e17ed
hw1010 <hyewon6588@naver.com>
2023-10-16 22:28:28 -0400

    added controller files

---
+++ server/controllers/category.controller.js
@@ -0,0 +1,10 @@
+import Category from '../models/category.model.js'
+import extend from 'lodash/extend'
+import errorHandler from './error.controller'
+const create = (req, res, next) => { }
+const list = (req, res) => { }
+const categoryByID = (req, res, next, id) => {  }
+const read = (req, res) => { }
+const update = (req, res, next) => {  }
+const remove = (req, res, next) => {  }
+export default { create, categoryByID, read, list, remove, update }
---
+++ server/controllers/product.controller.js
@@ -0,0 +1,10 @@
+import Product from '../models/product.model.js'
+import extend from 'lodash/extend'
+import errorHandler from './error.controller'
+const create = (req, res, next) => { }
+const list = (req, res) => { }
+const productByID = (req, res, next, id) => {  }
+const read = (req, res) => { }
+const update = (req, res, next) => {  }
+const remove = (req, res, next) => {  }
+export default { create, productByID, read, list, remove, update }
