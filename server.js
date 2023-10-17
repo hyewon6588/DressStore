e2330b366399cbdb2b24b2263bed528fe940b52d
hw1010 <hyewon6588@naver.com>
2023-10-16 20:11:39 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import config from './config/config.js' 
 import app from './server/express.js'
+import mongoose from 'mongoose' 
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true,
+useCreateIndex: true, 
+useUnifiedTopology: true } )
+mongoose.connection.on('error', () => {
+throw new Error(`unable to connect to database: ${mongoUri}`) 
+})
 app.get("/", (req, res) => {
-res.json({message:"Welcome to DressStore application."});
+res.json({ message: "Welcome to DressStore application." });
 });
 app.listen(config.port, (err) => { 
 if (err) {
